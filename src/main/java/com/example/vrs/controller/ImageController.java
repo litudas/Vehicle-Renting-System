e258fcbad14ec4c6346e35250b4c0beb56543751
litudas <lalatendudas2001@gmail.com>
2024-12-12 14:43:41 +0530

    Configuring Image

--- src/main/java/com/example/vrs/controller/ImageController.java
+++ src/main/java/com/example/vrs/controller/ImageController.java
@@ -1,13 +1,19 @@
 package com.example.vrs.controller;
 
+import javax.print.attribute.standard.Media;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.vrs.entity.Image;
 import com.example.vrs.service.ImageService;
+import com.example.vrs.util.ResponseStructure;
 import com.example.vrs.util.SimpleResponseStructure;
 
 @RestController
@@ -29,4 +35,25 @@ public class ImageController {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(SimpleResponseStructure.create(HttpStatus.CREATED.value(), "Profile Picture Updated"));
 	}
+	
+	@GetMapping("/find-image-by-id")
+	public ResponseEntity<byte[]> findImageById(@RequestParam ("imageId") int imageId){
+
+		Image image=imageService.findImageById(imageId);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).contentType(MediaType.valueOf(image.getContentType()))
+				.body(image.getImageBytes());
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
